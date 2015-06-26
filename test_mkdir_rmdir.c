9f97160ed331761068c9564f55b79e619376533d
gianei <gianei.sebastiany@gmail.com>
2015-06-25 21:47:09 -0300

    Teste read seek e fix teste mk rm dir

--- test_mkdir_rmdir.c
+++ test_mkdir_rmdir.c
@@ -11,6 +11,9 @@ void ls(DIR2 dirHandle)
 }
 
 int main(int argc, char *argv[]) {
+    DIR2 handle = opendir2("/");
+    printf("Raiz antes de criar\n");
+    ls(handle);
     DIR2 handleDir1 = mkdir2("Dir1");
     DIR2 handleDir2 = mkdir2("Dir2");
     DIR2 handleDir3 = mkdir2("Dir2");
@@ -29,7 +32,29 @@ int main(int argc, char *argv[]) {
     if (handleDir3 < 0)
         printf("Erro não identificado! Deve permitir nome de arquivo com barras?\n");
 
-    DIR2 handle = opendir2("/");
+    printf("Raiz depois de criar\n");
+    handle = opendir2("/"); //handle precisa ler de novo pra resetar o pointer dele
     ls(handle);
+
+    if (rmdir2("Dir1") < 0){
+        printf("Nao conseguiu apagar\n");
+    }
+
+    if (rmdir2("Dir2") < 0){
+        rmdir2("Nao conseguiu apagar\n");
+    }
+
+    if (rmdir2("Dir3") >= 0){
+        printf("Apagou algo inexistente\n");
+    }
+
+    if (rmdir2("Dir4") >= 0){
+        printf("Apagou algo inexistente\n");
+    }
+
+    printf("Raiz depois de apagar\n");
+    handle = opendir2("/");
+    ls(handle);
+
     return 0;
 }
---
+++ test_read_seek.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "./include/t2fs.h"
+
+void ls(DIR2 dirHandle)
+{
+    DIRENT2 dirEntry;
+
+    while(readdir2(dirHandle, &dirEntry) >= 0) {
+        printf("Name: %s Type: %d Size: %lu\n", dirEntry.name, dirEntry.fileType, dirEntry.fileSize);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    FILE2 file = create2("bigfile");
+    int count;
+    char str[9];
+    if (file >= 0) {
+        for (count = 0; count < 50; count++) {
+            sprintf(str, "%08d", count);
+            write2(file, str, 8);
+        }
+    } else {
+        file = open2("bigfile");
+    }
+    seek2(file, 0);
+    read2(file, str, 8);
+    str[9] = '\0';
+    printf("%s\n", str);
+    seek2(file, 8*40);
+    read2(file, str, 8);
+    str[9] = '\0';
+    printf("Deve ser 39: %s\n", str);
+
+    close2(file);
+
+    return 0;
+}
